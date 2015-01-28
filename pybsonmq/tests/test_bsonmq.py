a50640ca784aeaa43db4c9cfde0781d1c8f83c29
Steven Silvester <steven.silvester@ieee.org>
2015-01-28 16:27:16 -0600

    Update test to use logging

--- pybsonmq/tests/test_bsonmq.py
+++ pybsonmq/tests/test_bsonmq.py
@@ -1,5 +1,10 @@
 from .. import DZMQ
 import time
+import logging
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
 
 
 class TestPubSub(object):
@@ -7,19 +12,32 @@ class TestPubSub(object):
     def setup(self):
         self.pub = DZMQ()
         self.sub = DZMQ()
+        self.sub.log.setLevel(logging.DEBUG)
+        sobj = StringIO()
+        hdlr = logging.StreamHandler(sobj)
+        hdlr.setLevel(logging.DEBUG)
+        self.sub.log.addHandler(hdlr)
+        self.io_hdlr = hdlr
 
     def test_basic(self):
         self.pub.advertise('what_what')
         time.sleep(1.0)
+        payload = {'foo': 'bar'}
 
         def cb(topic, msg):
             assert topic == 'what_what'
-            raise  # TODO: use logging here
+            assert msg == payload
 
         self.sub.subscribe('what_what', cb)
         self.sub.spinOnce()
 
-        self.pub.publish('what_what', {'foo': 'bar'})
+        self.pub.publish('what_what', payload)
         time.sleep(1.)
-
         self.sub.spinOnce()
+
+        # check the output
+        output = self.io_hdlr.stream.getvalue().strip()
+        assert "Connected to" in output
+        assert "Got message: what_what" in output
+
+        self.io_hdlr.stream.truncate(0)
